908099648b14290fe5a2b01e0219aff24a7f84cd
Amanda Ramos <amandaramosrocha2003@gmail.com>
2022-12-06 18:12:08 -0300

    Feature: created login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,13 @@ const Login = () => {
   return (
     <div className='auth'>
         <h1>Login</h1>
+        <form >
+            <input required type="text" placeholder='Nome de usuário' />
+            <input required type="password" placeholder='Senha' />
+            <button>Login</button>
+            <p>Isto é um erro</p>
+            <span>Ainda não tem uma conta? <Link to='/register'>Registre-se</Link> </span>
+        </form>
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,6 +5,15 @@ const Register = () => {
   return (
     <div className='auth'>
         <h1>Registre-se</h1>
+        <form >
+            <input required type="text" placeholder='Nome de usuário' />
+            <input required type="text" placeholder='E-mail' />
+            <input required type="password" placeholder='Senha' />
+            <input required type="password" placeholder='Confirme a senha' />
+            <button>Registrar</button>
+            <p>Isto é um erro</p>
+            <span>Já tem uma conta? <Link to='/login'>Login</Link> </span>
+        </form>
     </div>
   )
 }
